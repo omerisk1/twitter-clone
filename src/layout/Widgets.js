2609a0e8d45532c76774e2547d8ea7ef679bf7a6
Ömer IŞIK <isikomer373@gmail.com>
2022-03-18 13:03:16 +0300

    Timeline searchbox oluşturuldu.

--- src/layout/Widgets.js
+++ src/layout/Widgets.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { SearchIcon } from '../icon/icon'
 
 function Widgets() {
   return (
     <aside className="bg-purple-200 w-80">
-        <h1>Timeline</h1>
+        <div className="flex items-center space-x-2 p-3 m-3 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base">
+          <SearchIcon className=""/>
+          <input 
+              type="text" 
+              placeHolder="Search Twitter" 
+              className=" placeolder-gray-dark bg-transparent focus:outline-none w-full text-sm"></input>
+        </div>
+        <div>Timeline</div>
     </aside>
   )
 }
